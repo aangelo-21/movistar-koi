e18a790e5555fc8131855c3c68ccda5ecdc1014a
aangelo-21 <martinezglielmo@gmail.com>
2025-02-20 16:13:59 +0000

    routes done and added css to header buttons

--- src/components/header-buttons/HeaderButtons.jsx
+++ src/components/header-buttons/HeaderButtons.jsx
@@ -1,13 +1,18 @@
 import './HeaderButtons.css'
 
 function HeaderButtons() {
+
+  const handleNavigate = (page) => {
+    window.location.href = `/${page}`;
+  }
+
   return (
     <>
       <div className="header-buttons-container">
-        <b className="header-button">Inicio</b>
-        <b className="header-button">Equipos</b>
-        <b className="header-button">Calendario</b>
-        <b className="header-button">Sobre nosotros</b>
+        <b className="header-button" onClick={() => handleNavigate('home')}>Inicio</b>
+        <b className="header-button" onClick={() => handleNavigate('teams')}>Equipos</b>
+        <b className="header-button" onClick={() => handleNavigate('calendar')}>Calendario</b>
+        <b className="header-button" onClick={() => handleNavigate('about-us')}>Sobre nosotros</b>
       </div>
     </>
   )
