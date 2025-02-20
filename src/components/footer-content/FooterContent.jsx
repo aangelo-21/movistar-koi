29b2792f775e14c291fbbc9962bf924a9a4c8143
aangelo-21 <martinezglielmo@gmail.com>
2025-02-20 17:13:56 +0000

    updated footer content

--- src/components/footer-content/FooterContent.jsx
+++ src/components/footer-content/FooterContent.jsx
@@ -5,10 +5,10 @@ function FooterContent() {
   return (
     <>
       <div className="footer-content">
-        <b className="footer-content-item">Eslogan</b>
+        <b className="footer-content-item">#SomosMovistarKOI</b>
         <div className="footer-content-item">
-          <b className="footer-content-subitem">Telefono</b>
-          <b className="footer-content-subitem">Ubicacion</b>
+          <b className="footer-content-subitem">Teléfono: 913184670</b>
+          <b className="footer-content-subitem">Ubicación: <a href="https://maps.app.goo.gl/ZxVTzjgEaZ3YvNP4A">P.º de la Chopera, 14, Arganzuela, 28045 Madrid</a></b>
         </div>
       </div>
     </>
